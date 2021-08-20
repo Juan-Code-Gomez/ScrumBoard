2d52b6477fae919769c0798fc165159821b7f6e7
Juan-Code-Gomez <jgomezberrio054@gmail.com>
2021-08-19 21:43:39 -0500

    Se agrega la seguridad y autenticacion de token con sus validaciones para los usuarios admin y user

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const { dbConnection } = require("./db/db");
 const Role = require("./routers/role");
 const User = require("./routers/user");
+const Auth = require("./routers/auth");
+const Board = require("./routers/board");
 require("dotenv").config();
 
 const app = express();
@@ -11,6 +13,8 @@ app.use(express.json());
 app.use(cors());
 app.use("/api/role", Role);
 app.use("/api/user", User);
+app.use("/api/auth/", Auth);
+app.use("/api/board", Board);
 
 app.listen(process.env.PORT, () =>
   console.log("Backend server running OK, on port: ", process.env.PORT)
--- backend/models/board.js
+++ backend/models/board.js
@@ -1,6 +1,5 @@
 const moongose = require("mongoose");
-const jwt = require("jsonwebtoken");
-const moment = require("moment");
+
 
 const boardSchema = new moongose.Schema({
   userId: { type: moongose.Schema.ObjectId, ref: "user" },
@@ -11,17 +10,5 @@ const boardSchema = new moongose.Schema({
   date: { type: Date, default: Date.now },
 });
 
-userSchema.methods.generateJWT = function () {
-  return jwt.sign(
-    {
-      _id: this._id.Date,
-      name: this.name,
-      iat: moment().unix(),
-    },
-    process.env.SECRET_KEY_JWT
-  );
-};
-
 const board = moongose.model("board", boardSchema);
-
 module.exports = board;
